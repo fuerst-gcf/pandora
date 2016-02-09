79c91b68bcf8b6179e812369b3a586edb0b26d69
Steffen Fuerst (GCF) <steffen.fuerst@globalclimateforum.org>
2016-02-09 11:58:00 +0100

    added two compilation scripts (with a local environment)

---
+++ compile-release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/hdf5/target/lib/
+export PANDORAPATH=$HOME/pandora/target
+export PATH=$PATH:$PANDORAPATH/bin/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PANDORAPATH/lib/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/openmpi/lib
+export PATH=$PATH:/opt/openmpi/bin
+
+cd $HOME/pandora
+rm -r ./lib
+rm -r ./target/lib/libpandora.*
+scons hdf5=$HOME/hdf5/target/ -j 8
+scons install installDir=$HOME/pandora/target hdf5=$HOME/hdf5/target
